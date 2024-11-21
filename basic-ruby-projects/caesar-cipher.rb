41b71a5d20164eac04e23ee3939091d7172abf33
Youssef El Atmani <shihaabyoussef1447@gmail.com>
2024-11-21 10:58:10 +0000

    Create file of caesar-cipher project
    Also add the problem at the beginning of the file as a comment

---
+++ basic-ruby-projects/caesar-cipher.rb
@@ -0,0 +1,12 @@
+###################################### The assignment ######################################
+=begin
+Implement a caesar cipher that takes in a string and the shift factor and then outputs the modified string:
+
+  > caesar_cipher("What a string!", 5)
+  => "Bmfy f xywnsl!"
+
+You will need to remember how to convert a string into a number.
+Don’t forget to wrap from z to a.
+Don’t forget to keep the same case.
+=end
+###############################################################################################
